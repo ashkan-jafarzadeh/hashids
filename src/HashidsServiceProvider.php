11cacc4b77e7a1882be681505a13abe766476dea
Taha Kamkar <chieftaha@gmail.com>
2020-06-03 23:27:42 +0430

    provide configs.

--- src/HashidsServiceProvider.php
+++ src/HashidsServiceProvider.php
@@ -14,6 +14,8 @@ class HashidsServiceProvider extends ServiceProvider
     public function boot()
     {
         require_once __DIR__ . DIRECTORY_SEPARATOR . "helpers.php";
+
+        $this->registerConfigs();
     }
 
 
@@ -26,5 +28,21 @@ class HashidsServiceProvider extends ServiceProvider
     public function register()
     {
         //
+    }
+
+
+
+    /**
+     * register configs, both for autoload and vendor:publish
+     *
+     * @return void
+     */
+    private function registerConfigs()
+    {
+        $original_path = __DIR__ . DIRECTORY_SEPARATOR . 'configs.php';
+
+        $this->mergeConfigFrom($original_path, 'hashids');
+        $this->publishes([$original_path => config_path("hashids.php")]);
+
     }
 }
---
+++ src/configs.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Salt
+    |--------------------------------------------------------------------------
+    |
+    | These are the salts of encryption. While the helper function use `main`
+    | as default salt, you can always pass custom salts too. Just be sure
+    | to define salts in your .env file and keep them unique per app.
+    |
+    */
+
+    'salt'   => [
+        "main" => env("HASHID_SALT_MAIN", null),
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Bypass
+    |--------------------------------------------------------------------------
+    |
+    | This is useful when you are in debug/development mode and you got sick
+    | of constantly converting hashids and ids to each other and see what
+    | is what. You cam bypass mechanism by .env file to see real ids.
+    |
+    */
+
+    "bypass" => (bool)env("HASHID_BYPASS", false),
+];
