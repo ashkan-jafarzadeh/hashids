a851db66cd481fb83168738f49776eaacf4ee98f
Taha Kamkar <chieftaha@gmail.com>
2020-06-05 16:39:40 +0430

    add mdoel trait.

---
+++ src/HashidsModelTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Hashids;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait HashidsModelTrait
+{
+
+    /**
+     * add a select by hashid to the query builder.
+     *
+     * @param Builder $builder
+     * @param array   $hashids
+     *
+     * @return void
+     */
+    public function scopeWithHashids(Builder $builder, $hashids)
+    {
+        $builder->whereIn('id', hashid($hashids));
+    }
+
+
+
+    /**
+     * get Hashid accessor.
+     *
+     * @return string
+     */
+    public function getHashidAttribute()
+    {
+        return hashid($this->id);
+    }
+}
